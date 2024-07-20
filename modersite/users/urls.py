92006158cfdc1ff6249fc38274d76cd5c6eaadd6
d9pouces <github@19pouces.net>
2024-07-20 20:12:08 +0200

    feat: start of django-postman

--- modersite/users/urls.py
+++ modersite/users/urls.py
@@ -2,10 +2,11 @@
 
 from django.urls import path
 
-from modersite.users.views import UserSettingsView, theme_switch
+from modersite.users.views import MessageUpdateView, UserSettingsView, theme_switch
 
 app_name = "users"
 urlpatterns = [
     path("theme-switch", theme_switch, name="theme_switch"),
     path("settings", UserSettingsView.as_view(), name="settings"),
+    path("update-message", MessageUpdateView.as_view(), name="update-message"),
 ]
--- modersite/users/views.py
+++ modersite/users/views.py
@@ -2,12 +2,18 @@
 
 from django.conf import settings
 from django.contrib import messages
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.core.exceptions import PermissionDenied
+from django.db.models import Q
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from django.utils.http import url_has_allowed_host_and_scheme, urlencode
+from django.utils.timezone import now
 from django.utils.translation import gettext as _
 from django.views.decorators.cache import never_cache
-from django.views.generic import FormView
+from django.views.generic import FormView, View
+from postman.models import Message
+from postman.views import UpdateDualMixin
 
 from modersite.user_settings import set_user_setting
 from modersite.users.forms import UserSettingsForm
@@ -34,6 +40,61 @@ class UserSettingsView(FormView):
         form.save()
         messages.success(self.request, _("Your settings have been saved."))
         return super().form_valid(form)
+
+
+class MessageUpdateView(UpdateDualMixin, View):
+    """View for updating messages or conversations.
+
+    This single view replaces the following views: `ArchiveView`, `DeleteView`,
+    `UndeleteView`, 'MarkReadView', 'MarkUnreadView'.
+    This is required to avoid the onclick attribute in the template (forbidden by CSP).
+    """
+
+    @property
+    def selected_action(self):
+        """Get the action to perform on the selected messages or conversations."""
+        action = self.request.POST.get("action")
+        if action not in {"delete", "archive", "undelete", "read", "unread"}:
+            raise PermissionDenied
+        return action
+
+    def _action(self, user: AbstractBaseUser, filter_: Q):
+        """Perform the action on the selected messages or conversations."""
+        if self.selected_action in {"read", "unread"}:
+            Message.objects.as_recipient(user, filter_).filter(
+                **{f"{self.field_bit}__isnull": bool(self.field_value)}
+            ).update(**{self.field_bit: self.field_value})
+        else:
+            super()._action(user, filter_)
+        # an empty set cannot be estimated as an error, it may be just a badly chosen selection
+
+    @property
+    def field_bit(self):
+        """Get the field to update."""
+        return {
+            "delete": "deleted_at",
+            "archive": "archived",
+            "undelete": "deleted_at",
+            "read": "read_at",
+            "unread": "read_at",
+        }[self.selected_action]
+
+    @property
+    def success_msg(self):
+        """Get the success message to display."""
+        return {
+            "delete": _("Messages or conversations successfully deleted."),
+            "archive": _("Messages or conversations successfully archived."),
+            "undelete": _("Messages or conversations successfully recovered."),
+            "read": _("Messages or conversations successfully marked as read."),
+            "unread": _("Messages or conversations successfully marked as unread."),
+        }[self.selected_action]
+
+    @property
+    def field_value(self):
+        """Get the value to set the field to."""
+        n = now()
+        return {"delete": n, "archive": True, "undelete": None, "read": n, "unread": None}[self.selected_action]
 
 
 @never_cache
